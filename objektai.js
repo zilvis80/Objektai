fe31c11df25a7d8f9f25f4c99d4fcb270d0667e2
zilvinas <zilvis80@mail.ru>
2024-01-16 22:11:35 +0200

    first commit

---
+++ objektai.js
@@ -0,0 +1,67 @@
+// let studentas = {
+//   vardas: 'Tomas',
+//   pavarde: 'Tomaitis',
+//   amzius: 25,
+//   ugis: 200,
+//   kursas: 3,
+//   mokykla: 'Universitetas',
+// };
+// console.log(studentas);
+// console.log(studentas.pavarde);
+// console.log(studentas.mokykla);
+
+// let Filmas = {
+//   pavadinimas: 'drambliai',
+//   rezisierius: 'Marijanas',
+//   Biudzetas: 100000,
+//   grynasPelnas: 50000,
+//   zanras: 'siaubo',
+//   trukme: '3 val',
+//   isleidimoMetai: 2023,
+//   aktoriai: ['bronius', 'paulius', 'Silvija'],
+// };
+// console.log(Filmas.aktoriai.length);
+
+// let knygos1 = {
+//   pavadinimas: 'vasara',
+//   autorius: 'Mykolas',
+//   zanras: 'nuotykiu',
+//   kaina: 32,
+//   puslapiuSkaicius: 623,
+//   skyriai: ['pradzia', 'pabegimas', 'vyduris', 'pabaiga', 'isvados'],
+//   isleidimoMetai: 1983,
+//   knyginas: 'yra',
+// };
+// let knygos2 = {
+//   pavadinimas: 'ziema',
+//   autorius: 'Bronius',
+//   zanras: 'detektyvas',
+//   kaina: 15,
+//   puslapiuSkaicius: 564,
+//   skyriai: ['pradzia', 'pabegimas', 'vydurys', 'pabaiga', 'isvados'],
+//   isleidimoMetai: 1954,
+//   knyginas: 'nera',
+// };
+// console.log(knygos1);
+// console.log(knygos2);
+// if (knygos1.puslapiuSkaicius > knygos2.puslapiuSkaicius) {
+//   console.log('antra knyga plonesne');
+// } else if (knygos1.puslapiuSkaicius < knygos2.puslapiuSkaicius) {
+//   console.log('pirma knyga plonesne');
+// } else {
+//   console.log('abi knygos vienodos');
+// }
+// if (knygos1.skyriai.length > knygos2.skyriai.length) {
+//   console.log('pimoj skyriu daugiau');
+// } else if (knygos1.skyriai.length < knygos2.skyriai.length) {
+//   console.log('antroj skyriu daugiau');
+// } else {
+//   console.log('skyriu vienodai');
+// }
+// if (knygos2.kaina * 2 > knygos1.kaina) {
+//   console.log('ar pirma knyga brangesne');
+// } else if (knygos2.kaina * 2 < knygos1.kaina) {
+//   console.log('ar antra knyga brangesne');
+// } else {
+//   console.log('kainos vienodos');
+// }
