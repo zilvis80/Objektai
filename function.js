270b3073e06b54ee307f6c14618e402eb75c5601
zilvinas <zilvis80@mail.ru>
2024-01-22 22:48:56 +0200

    uzduotys

--- function.js
+++ function.js
@@ -49,7 +49,7 @@
 // function veiksmas(zodziai) {
 //   console.log(zodziai);
 //   for (let i = 0; i < zodziai.length; i++) {
-//     console.log(zodziai[0].length);
+//     console.log(zodziai[i].length);
 //   }
 // }
 // veiksmas(['zodis1', 'zodziai2', 'zodinelsi3']);
@@ -63,3 +63,81 @@
 //   console.log(skaiciai);
 // }
 // randomSkaiciai();
+
+// function suformuotas_sakinys(vardas, profesija, amzius) {
+//   return `${vardas} ${profesija} metai (${amzius})`;
+// }
+// console.log(suformuotas_sakinys('giedrius', 'Valytojas', 65));
+
+// 4 dalis 2 uzduotys
+
+// function randomSkaiciai() {
+//   let skaiciai = [];
+//   for (let i = 1; i <= 10; i++) {
+//     let x = Math.ceil(Math.random() * 10);
+//     skaiciai.push(x);
+//   }
+//   console.log(skaiciai);
+// }
+// randomSkaiciai();
+
+// function suma(a, b, c) {
+//   return a * b * c;
+// }
+// function atsakymas(a, b, c) {
+//   console.log(`${a}*${b}*${c}=${suma(a, b, c)}`);
+// }
+// let ska1 = 6;
+// let ska2 = 8;
+// let ska3 = 9;
+// atsakymas(ska1, ska2, ska3);
+
+// atsakymas(3, 5, 9);
+// atsakymas(5, 7, 3);
+
+// function eilerastis() {
+//   console.log('Žali pušynėliai, beržynai,');
+//   console.log('Melsvi ežerai — ir arti, ir toli...');
+//   console.log('Prie Nemuno — mano tėvynė,');
+//   console.log('Dainų ir artojų šalis.');
+//   console.log('Palaukėm takeliai nubėga,');
+//   console.log();
+// }
+// eilerastis();
+// eilerastis();
+// eilerastis();
+// eilerastis();
+// eilerastis();
+
+// function mano(vardas, patinka) {
+//   return `${vardas} ${patinka}`;
+// }
+// console.log(mano('Zilvinas', 'nes noriu pakeist profesiaj'));
+// console.log(mano('Zilvinas', 'nes noriu pakeist profesiaj'));
+// console.log(mano('Zilvinas', 'nes noriu pakeist profesiaj'));
+
+// function vienas() {
+//   console.log('Programavimas jega');
+// }
+// function dvejetas() {
+//   console.log('As labai noriu programuot');
+// }
+// function trejetas() {
+//   console.log('noriu labai susirast darba');
+// }
+
+// vienas();
+// dvejetas();
+// trejetas();
+
+// function pirma() {
+//   console.log('viskas labai gerai');
+// }
+// function antra() {
+//   console.log('sitame pasaulyje');
+// }
+// function trecia() {
+//   pirma();
+//   antra();
+// }
+// trecia();
